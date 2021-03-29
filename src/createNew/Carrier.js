afed6b71595c4aee755404552710aa68ca761282
Jawad <jawadbanga@gmail.com>
2021-03-28 18:20:43 -0700

    Adding carrier

--- src/createNew/Carrier.js
+++ src/createNew/Carrier.js
@@ -1,9 +1,16 @@
-
+import { Form } from 'react-bootstrap'
+const choices = [{name: 'USPS'}, {name: 'DHL'}, {name: 'UPS'}, {name: 'FedEx'}]
 
 function CarrierChoices() {
     return (
-        <div>
-            
-        </div>
+        <Form.Control as="select" custom>
+        {
+            choices.map((item, index) => {
+                return <option id={index}>{item.name}</option>
+            })
+        }
+        </Form.Control>
     )
 }
+
+export default CarrierChoices;
